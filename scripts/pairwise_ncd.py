6031d96c75c158bca5a630a5712da56a5a7f28f0
Benjamin Lee <benjamindlee@me.com>
2018-10-12 13:50:42 -0700

    Cleanup and annotations for pairwise ncd

--- scripts/pairwise_ncd.py
+++ scripts/pairwise_ncd.py
@@ -11,18 +11,7 @@ import bz2
 import gzip
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-x', '--seq_x', help="sequence 1",
-                        type=str, required=True)
-    parser.add_argument('-y', '--seq_y', help="sequence 2",
-                        type=str, required=True)
-    parser.add_argument('-c', '--compression', help="compression algorithm",
-                        type=str, choices=['lzma', 'gzip', 'bzip2', 'zlib', 'lz4'], required=True)
-    args = parser.parse_args()
-    return args
-
-def bwt(string):
+def bwt(s: str) -> str:
     """
     Code for Burrows-Wheeler Transformation. The code is adapted from Rosetta Code.
     """
@@ -33,11 +22,10 @@ def bwt(string):
 
 
 #given 2 sequence strings, returns sequences + concatenation as an object of bytes
-def return_byte(sequence1, sequence2):
+def return_byte(sequence1: str, sequence2: str) -> tuple:
     seq1 = bytes(sequence1, 'utf-8')
     seq2 = bytes(sequence2, 'utf-8')
-    seqconcat = concat(seq1, seq2)
-    return(seq1, seq2, seqconcat)
+    return seq1, seq2, seq1 + seq2
 
 
 def compressed_size(sequences: tuple, algorithm: str) -> tuple:
@@ -73,7 +61,7 @@ def compressed_size(sequences: tuple, algorithm: str) -> tuple:
     return compressed_seq1_size, compressed_seq2_size, compressed_seqconcat_size
 
 #calculates NCD for 2 sequence sizes and their concatenation size
-def compute_distance(x, y, cxy):
+def compute_distance(x: float, y: float, cxy: float) -> float:
     if x > y:
         distance = ((cxy - y) / x)
     elif y > x:
@@ -81,53 +69,3 @@ def compute_distance(x, y, cxy):
     else:
         distance = ((cxy - x) / x)
     return distance
-
-#concatenates two input sequences together
-def concat(sequence1, sequence2):
-    concat_genome = sequence1 + sequence2
-    return concat_genome
-
-
-def main(algorithm, seq_x, seq_y):
-    #parse command line arguments
-    inputfile1 = seq_x
-    inputfile2 = seq_y
-    outputfile = ''
-
-    #error conditions: missing input and confliciting command line arguments
-    if inputfile1 == '' or inputfile2 == '':
-        print("Error: Missing input")
-        sys.exit(3)
-
-    #open input sequences, exits if file not found
-    try:
-        with open(inputfile1, "r") as myfile:
-            seq1 = myfile.read()
-    except:
-        print('Error reading sequence 1')
-        sys.exit(3)
-
-    try:
-        with open(inputfile2, "r") as myfile:
-            seq2 = myfile.read()
-    except:
-        print('Error reading sequence 2')
-        sys.exit(3)
-
-    #convert input sequences into bytes
-    sequences = return_byte(seq1, seq2)
-
-    #compress input sequences
-    sizes = compressed_size(sequences, algorithm)
-
-    #compute ncd values
-    ncd = compute_distance(sizes[0], sizes[1], sizes[2])
-    print(ncd)
-
-
-if __name__ == "__main__":
-    args = parse_arguments()
-    algorithm = args.compression
-    seq_x = args.seq_x
-    seq_y = args.seq_y
-    main(algorithm, seq_x, seq_y)
