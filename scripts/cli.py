da60754bff81ebec24ae585ce64ddd5d7c260582
William Casazza <willcasazza@gmail.com>
2018-10-12 13:23:26 -0700

    parallel part seems to be working, I/O is subject

--- scripts/cli.py
+++ scripts/cli.py
@@ -7,7 +7,7 @@ import pandas as pd
 
 from pairwise_ncd import return_byte, compressed_size, compute_distance
 
-def compute_distance(comparison, algorithm= "bzip2"):
+def compute_distance(comparison, algorithm= "lzma"):
     #Compute a distance between a and b
     sequences = return_byte(open(comparison[0].read()), open(comparison[1].read()))
     sizes = compressed_size(sequences, algorithm=algorithm)
@@ -18,7 +18,8 @@ def compute_distance(comparison, algorithm= "bzip2"):
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option("-f", "--fasta", type=click.Path(dir_okay=False, exists=True, resolve_path=True), multiple=True, help="FASTA file containing sequence to compare")
 @click.option("-d", "--directory", "directories", type=click.Path(dir_okay=True, file_okay=False, exists=True, resolve_path=True), multiple=True, help="Directory containing FASTA files to compare")
-def cli(fasta, directories):
+@click.option("-N", "--numCPU", "numCPU",default= 1, help="Number of CPU cores available")
+def cli(fasta, directories, numCPU):
 
     # generate a list of absolute paths containing the files to be compared
     files = list(fasta)
@@ -29,8 +30,9 @@ def cli(fasta, directories):
                 files.append(os.path.abspath(os.path.join(dirpath, f)))
     files = list(set(files)) # remove duplicates
     comparisons = tqdm(list(product(files, repeat=2)))
-    with concurrent.futures.ProcessPoolExecutor as executor:
-        distances = executor.map(compute_distance,comparisons)
+
+    executor = concurrent.futures.ProcessPoolExecutor(max_workers=numCPU)
+    distances = [res for res in executor.map(compute_distance,comparisons)]
 
     # for comparison in tqdm(list(product(files, repeat=2))):
     #     #convert input sequences into bytes
