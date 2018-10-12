bd60c3d1f3846d734949af8c2defb106c04d7872
William Casazza <willcasazza@gmail.com>
2018-10-12 13:06:40 -0700

    debugging parallelize

--- scripts/cli.py
+++ scripts/cli.py
@@ -1,10 +1,19 @@
 import click
 import os
+import concurrent.futures
 from itertools import product
 from tqdm import tqdm
 import pandas as pd
 
 from pairwise_ncd import return_byte, compressed_size, compute_distance
+
+def compute_distance(comparison, algorithm= "bzip2"):
+    #Compute a distance between a and b
+    sequences = return_byte(open(comparison[0].read()), open(comparison[1].read()))
+    sizes = compressed_size(sequences, algorithm=algorithm)
+    ncd = compute_distance(sizes[0], sizes[1], sizes[2])
+    return (comparison[0], comparison[1], ncd)
+
 
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option("-f", "--fasta", type=click.Path(dir_okay=False, exists=True, resolve_path=True), multiple=True, help="FASTA file containing sequence to compare")
@@ -19,20 +28,21 @@ def cli(fasta, directories):
             for f in filenames:
                 files.append(os.path.abspath(os.path.join(dirpath, f)))
     files = list(set(files)) # remove duplicates
+    comparisons = tqdm(list(product(files, repeat=2)))
+    with concurrent.futures.ProcessPoolExecutor as executor:
+        distances = executor.map(compute_distance,comparisons)
 
-    distances = []
-
-    for comparison in tqdm(list(product(files, repeat=2))):
-        #convert input sequences into bytes
-        sequences = return_byte(open(comparison[0]).read(), open(comparison[1]).read())
-
-        #compress input sequences
-        sizes = compressed_size(sequences, algorithm="bzip2")
-
-        #compute ncd values
-        ncd = compute_distance(sizes[0], sizes[1], sizes[2])
-
-        distances.append((comparison[0], comparison[1], ncd))
+    # for comparison in tqdm(list(product(files, repeat=2))):
+    #     #convert input sequences into bytes
+    #     sequences = return_byte(open(comparison[0]).read(), open(comparison[1]).read())
+    #
+    #     #compress input sequences
+    #     sizes = compressed_size(sequences, algorithm="bzip2")
+    #
+    #     #compute ncd values
+    #     ncd = compute_distance(sizes[0], sizes[1], sizes[2])
+    #
+    #     distances.append((comparison[0], comparison[1], ncd))
 
     pd.DataFrame(distances, columns=["file1", "file2", "ncd"]).to_csv("out.csv", index=False)
 
